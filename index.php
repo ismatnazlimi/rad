83104c91f06a061264e18461224e20da6b08a174
Abuarif <suhaimi.albakri@gmail.com>
2018-01-09 11:34:17 +0800

    add index.php

---
+++ index.php
@@ -0,0 +1,250 @@
+<!DOCTYPE html>
+<html >
+<head>
+  <!-- Site made with Mobirise Website Builder v4.5.4, https://mobirise.com -->
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="generator" content="Mobirise v4.5.4, mobirise.com">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/rapid-rail-horizontal-full-colour-1414x472.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Home</title>
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons-bold/mobirise-icons-bold.css">
+  <link rel="stylesheet" href="assets/web/assets/mobirise-icons/mobirise-icons.css">
+  <link rel="stylesheet" href="assets/tether/tether.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/gallery/style.css">
+  <link rel="stylesheet" href="assets/mobirise/css/mbr-additional.css" type="text/css">
+  
+  
+  
+</head>
+<body>
+<section class="menu cid-qG6lQhQRoN" once="menu" id="menu1-e">
+
+    
+
+    <nav class="navbar navbar-expand beta-menu navbar-dropdown align-items-center navbar-fixed-top navbar-toggleable-sm">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <div class="hamburger">
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </button>
+        <div class="menu-logo">
+            <div class="navbar-brand">
+                <span class="navbar-logo">
+                    <a href="https://mobirise.com">
+                         <img src="assets/images/rapid-rail-horizontal-full-colour-1414x472.png" alt="Mobirise" title="" style="height: 3.8rem;">
+                    </a>
+                </span>
+                <span class="navbar-caption-wrap"><a class="navbar-caption text-secondary display-4" href="index.html#features8-j">
+                        Rail Academy</a></span>
+            </div>
+        </div>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item dropdown">
+                    <a class="nav-link link text-white display-7" href="#top">
+                        <span class="mbri-home mbr-iconfont mbr-iconfont-btn"></span>
+                        Home</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link link text-white dropdown-toggle display-7" href="https://mobirise.com" data-toggle="dropdown-submenu" aria-expanded="false"><span class="mbri-file mbr-iconfont mbr-iconfont-btn"></span>
+                        
+                        Course Calendar</a><div class="dropdown-menu"><a class="text-white dropdown-item display-7" href="assets/files/calendar_2018_agl.pdf" target="_blank">Ampang Line</a><a class="text-white dropdown-item display-7" href="assets/files/calendar_2018_kjl.pdf" aria-expanded="false" target="_blank">Kelana Jaya Line</a><a class="text-white dropdown-item display-7" href="assets/files/calendar_2018_mrl.pdf" aria-expanded="false" target="_blank">Monorail Line</a><a class="text-white dropdown-item display-7" href="assets/files/calendar_2018_mrt.pdf" aria-expanded="false" target="_blank">MRT-SBK Line</a><a class="text-white dropdown-item display-7" href="assets/files/calendar_2018_afc.pdf" aria-expanded="true" target="_blank">AFC</a></div>
+                </li><li class="nav-item"><a class="nav-link link text-white display-7" href="https://goo.gl/forms/wGdusxrP44G4pIN63" aria-expanded="false" target="_blank"><span class="mbri-edit mbr-iconfont mbr-iconfont-btn"></span>
+                        
+                        Course Evaluation</a></li></ul>
+            <div class="navbar-buttons mbr-section-btn"><a class="btn btn-sm btn-success display-4" href="https://rcms.prasarana.com.my/lms"><span class="mbri-key mbr-iconfont mbr-iconfont-btn"></span>
+                    </a></div>
+        </div>
+    </nav>
+</section>
+
+<section class="engine"><a href="https://mobirise.co/f">free bootstrap builder</a></section><section class="cid-qG6lQo6EHY mbr-fullscreen mbr-parallax-background" id="header2-f">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.5; background-color: rgb(35, 35, 35);"></div>
+
+    <div class="container align-center">
+        <div class="row justify-content-md-center">
+            <div class="mbr-white col-md-10">
+                <h1 class="mbr-section-title mbr-bold pb-3 mbr-fonts-style display-1">RAIL COMPETENCE <br>MANAGEMENT SYSTEM</h1>
+                <h3 class="mbr-section-subtitle align-center mbr-light pb-3 mbr-fonts-style display-2"></h3>
+                <p class="mbr-text pb-3 mbr-fonts-style display-5">
+                    Knowing is half the battle! ENROL our programme and professional course NOW.</p>
+                <div class="mbr-section-btn"><a class="btn btn-md btn-secondary display-4" href="https://rcms.prasarana.com.my/lms">LOGIN</a></div>
+            </div>
+        </div>
+    </div>
+    <div class="mbr-arrow hidden-sm-down" aria-hidden="true">
+        <a href="#next">
+            <i class="mbri-down mbr-iconfont"></i>
+        </a>
+    </div>
+</section>
+
+<section class="features8 cid-qG6uoJESwI mbr-parallax-background" id="features8-j">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.6; background-color: rgb(35, 35, 35);">
+    </div>
+
+    <div class="container">
+        <div class="media-container-row">
+
+            <div class="card  col-12 col-md-6 col-lg-4">
+                <div class="card-img">
+                    <span class="mbr-iconfont mbrib-users"></span>
+                </div>
+                <div class="card-box align-center">
+                    <h4 class="card-title mbr-fonts-style display-7">
+                        Who we are?</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">Training body responsible for Rail workforce training and development needs of Kelana Jaya Line, Ampang Line, Monorail Line and MRT SBK Line.&nbsp;<br></p>
+                    
+                </div>
+            </div>
+
+            <div class="card  col-12 col-md-6 col-lg-4">
+                <div class="card-img">
+                    <span class="mbr-iconfont mbrib-growing-chart"></span>
+                </div>
+                <div class="card-box align-center">
+                    <h4 class="card-title mbr-fonts-style display-7">
+                        Core Responsibility</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">To manage all systems and subsystems competency trainings including competency assessment, certification and licensing needs for staff required to operate and maintain the safety critical system and equipment.&nbsp;<br></p>
+                    
+                </div>
+            </div>
+
+            <div class="card  col-12 col-md-6 col-lg-4">
+                <div class="card-img">
+                    <span class="mbr-iconfont mbrib-database"></span>
+                </div>
+                <div class="card-box align-center">
+                    <h4 class="card-title mbr-fonts-style display-7">
+                        Facilities</h4>
+                    <p class="mbr-text mbr-fonts-style display-7">
+                       4 Training Centres located at respective depots</p>
+                    
+                </div>
+            </div>
+
+            
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section info2 cid-qG7Tk0eDUn" id="info2-q">
+
+    
+
+    <div class="mbr-overlay" style="opacity: 0.8; background-color: rgb(37, 170, 178);">
+    </div>
+
+    <div class="container">
+        <div class="row main justify-content-center">
+            <div class="media-container-column col-12 col-lg-3 col-md-4">
+                
+            </div>
+            <div class="media-container-column title col-12 col-lg-7 col-md-6">
+                <h2 class="align-right mbr-bold mbr-white pb-3 mbr-fonts-style display-2">RAIL ACADEMY</h2>
+                <h3 class="mbr-section-subtitle align-right mbr-light mbr-white mbr-fonts-style display-5">
+                    Our activities</h3>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-gallery mbr-slider-carousel cid-qG6Wnge2L3" id="gallery3-o">
+
+    
+
+    <div>
+        <div><!-- Filter --><!-- Gallery --><div class="mbr-gallery-row"><div class="mbr-gallery-layout-default"><div><div><div class="mbr-gallery-item mbr-gallery-item--pNaN" data-video-url="false" data-tags="Creative"><div href="#lb-gallery3-o" data-slide-to="0" data-toggle="modal"><img src="assets/images/rad06064-2000x1333-800x533.jpg" alt=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">4 Januari 2017 - Perlaksanaan PPT secara Latihan Amali</span></div></div><div class="mbr-gallery-item mbr-gallery-item--pNaN" data-video-url="false" data-tags="Awesome"><div href="#lb-gallery3-o" data-slide-to="1" data-toggle="modal"><img src="assets/images/rad05257-2000x1333-800x533.jpg" alt=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">18 Dec 2017 - Professional Metro Train Driver Training - MRT Jakarta Batch 3</span></div></div><div class="mbr-gallery-item mbr-gallery-item--pNaN" data-video-url="false" data-tags="Responsive"><div href="#lb-gallery3-o" data-slide-to="2" data-toggle="modal"><img src="assets/images/00000215-2000x1333-800x533.jpg" alt=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">12- December 2017 - &nbsp;Visit to Uniten (School of Engineering)</span></div></div><div class="mbr-gallery-item mbr-gallery-item--pNaN" data-video-url="false" data-tags="Awesome"><div href="#lb-gallery3-o" data-slide-to="3" data-toggle="modal"><img src="assets/images/tskm5012-2000x1333-800x533.jpg" alt=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">8 December 2017 - Program Induksi PP-PPT</span></div></div><div class="mbr-gallery-item mbr-gallery-item--pNaN" data-video-url="false" data-tags="Animated"><div href="#lb-gallery3-o" data-slide-to="4" data-toggle="modal"><img src="assets/images/ccm0496-2000x1333-800x533.jpg" alt=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">17 Nov 2017 -&nbsp;Annual Productivity &amp; Innovation Conference &amp; Exposition 2017</span></div></div><div class="mbr-gallery-item mbr-gallery-item--pNaN" data-video-url="false" data-tags="Responsive"><div href="#lb-gallery3-o" data-slide-to="5" data-toggle="modal"><img src="assets/images/iccm4419-2000x1333-800x533.jpg" alt=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">21 Nov 2017 - Certificate Giving Ceremony for UniKL Train The Trainer Programme</span></div></div><div class="mbr-gallery-item mbr-gallery-item--pNaN" data-video-url="false" data-tags="Animated"><div href="#lb-gallery3-o" data-slide-to="6" data-toggle="modal"><img src="assets/images/msmm0081-2000x1333-800x533.jpg" alt=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">2 Nov 2017 - SMILE Launching Ceremony</span></div></div><div class="mbr-gallery-item mbr-gallery-item--pNaN" data-video-url="false" data-tags="Awesome"><div href="#lb-gallery3-o" data-slide-to="7" data-toggle="modal"><img src="assets/images/rad09648-2000x1333-800x533.jpg" alt=""><span class="icon-focus"></span><span class="mbr-gallery-title mbr-fonts-style display-7">16 Oct 2017 - &nbsp;RISE &amp; SMILE Program for Station Manager (Session 5)</span></div></div></div></div><div class="clearfix"></div></div></div><!-- Lightbox --><div data-app-prevent-settings="" class="mbr-slider modal fade carousel slide" tabindex="-1" data-keyboard="true" data-interval="false" id="lb-gallery3-o"><div class="modal-dialog"><div class="modal-content"><div class="modal-body"><div class="carousel-inner"><div class="carousel-item"><img src="assets/images/rad06064-2000x1333.jpg" alt=""></div><div class="carousel-item"><img src="assets/images/rad05257-2000x1333.jpg" alt=""></div><div class="carousel-item"><img src="assets/images/00000215-2000x1333.jpg" alt=""></div><div class="carousel-item"><img src="assets/images/tskm5012-2000x1333.jpg" alt=""></div><div class="carousel-item"><img src="assets/images/ccm0496-2000x1333.jpg" alt=""></div><div class="carousel-item"><img src="assets/images/iccm4419-2000x1333.jpg" alt=""></div><div class="carousel-item active"><img src="assets/images/msmm0081-2000x1333.jpg" alt=""></div><div class="carousel-item"><img src="assets/images/rad09648-2000x1333.jpg" alt=""></div></div><a class="carousel-control carousel-control-prev" role="button" data-slide="prev" href="#lb-gallery3-o"><span class="mbri-left mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Previous</span></a><a class="carousel-control carousel-control-next" role="button" data-slide="next" href="#lb-gallery3-o"><span class="mbri-right mbr-iconfont" aria-hidden="true"></span><span class="sr-only">Next</span></a><a class="close" href="#" role="button" data-dismiss="modal"><span class="sr-only">Close</span></a></div></div></div></div></div>
+    </div>
+
+</section>
+
+<section class="cid-qG6lQq2Caz" id="footer1-g">
+
+    
+
+    
+
+    <div class="container">
+        <div class="media-container-row content text-white">
+            <div class="col-12 col-md-3">
+                <div class="media-wrap">
+                    <a href="https://mobirise.com/">
+                        <img src="assets/images/rapid-rail-vertical-full-colour-1147x1097.png" alt="Mobirise" title="">
+                    </a>
+                </div>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Address
+                </h5>
+                <p class="mbr-text">Kompleks Rapid Rail Subang<br>No.1 Jalan PJU 1A/46,<br>Off Jalan Lapangan Terbang Subang, 47301 Petaling Jaya, Selangor.</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3">
+                    Contacts
+                </h5>
+                <p class="mbr-text">Email : idris@prasarana.com.my<br>Phone: +60376507788 (2304)<br>Fax: +6076257608</p>
+            </div>
+            <div class="col-12 col-md-3 mbr-fonts-style display-7">
+                <h5 class="pb-3"></h5>
+                <p class="mbr-text"></p>
+            </div>
+        </div>
+        <div class="footer-lower">
+            <div class="media-container-row">
+                <div class="col-sm-12">
+                    <hr>
+                </div>
+            </div>
+            <div class="media-container-row mbr-white">
+                <div class="col-sm-6 copyright">
+                    <p class="mbr-text mbr-fonts-style display-7">
+                        © Copyright 2017 Mobirise - All Rights Reserved
+                    </p>
+                </div>
+                <div class="col-md-6">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/tether/tether.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/masonry/masonry.pkgd.min.js"></script>
+  <script src="assets/imagesloaded/imagesloaded.pkgd.min.js"></script>
+  <script src="assets/bootstrapcarouselswipe/bootstrap-carousel-swipe.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/vimeoplayer/jquery.mb.vimeo_player.js"></script>
+  <script src="assets/parallax/jarallax.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+  <script src="assets/slidervideo/script.js"></script>
+  <script src="assets/gallery/player.min.js"></script>
+  <script src="assets/gallery/script.js"></script>
+  
+  
+</body>
+</html>
